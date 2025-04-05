73acbc2cbb3dc7830f06045c3f6c362fafddebef
pvsailatha <180030350cse@gmail.com>
2025-04-05 16:53:18 -0500

    Initial commit

---
+++ src/pages/Index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Dashboard from "@/components/Dashboard";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen mental-gradient-bg">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Index;
